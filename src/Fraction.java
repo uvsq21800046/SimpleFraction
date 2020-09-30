6e4e414fd1c00fc31be4f02690587433b73a09c3
uvsq21800046 <natacha.doutreleau@ens.uvsq.fr>
2020-10-01 01:14:21 +0200

    Ajout fonction toString et test dans le main

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,2 +1,17 @@
 public class Fraction {
+    public double num;
+    public double den;
+    public double res;
+
+    public Fraction(double n, double d){
+        num= n;
+        den= d;
+        res= n/d;
+    }
+
+    public String toString(){
+        return String.valueOf(this.res);
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+
+    public static void main(String[] args){
+        Fraction f= new Fraction(3,2);
+        System.out.println(f.toString()+" "+f.toString().getClass());
+    }
+
+    
 }
